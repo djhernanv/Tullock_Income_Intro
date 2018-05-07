a262f87910d3d27462f759bf905b6a68efb5a6d7
Hernan Villamizar <hernan.villamizar@gmail.com>
2018-05-07 23:36:24 +0200

    Initial Upload
    This uploads all the initial files

---
+++ models.py
@@ -0,0 +1,206 @@
+from __future__ import division
+
+import numpy as np
+import string
+
+import otree.models
+from otree.db import models
+from otree import widgets
+from otree.common import Currency as c, currency_range, safe_json
+from otree.constants import BaseConstants
+from otree.models import BaseSubsession, BaseGroup, BasePlayer
+
+author = 'AS,GS,HV,KS'
+
+doc = """
+Real Effort Task - Letter Count
+This is the introduction to a Tullock Contest in form of a Real Effort Task
+It welcomes the participants and helps them trough the first stages
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'tullock_Intro_RET'
+    players_per_group = 3
+    num_rounds = 2  # first round is with low, second with high wage
+
+    t = 60  # Total Time in seconds available for both solving and staying in switch
+    time_in_minutes = t/60
+
+    tokensper_string = 1
+    tokensper_string_high = 2
+    eurosper_token = c(0.10)
+    secondsper_token = 10
+
+    increase_per_string = 4
+
+    # this is a summarized instruction to be shown under each sequence as a reminder:
+    instructions_summarized = 'Tullock_Intro_RET/InstructionsSum.html'
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_output = models.IntegerField(initial=0)  # define group variable to calculate probabilities
+
+    # determine total output
+
+    def set_total_output(self):
+        total_output = sum(p.output for p in self.get_players())  # retrieves a list of all individual outputs
+        # and sums them up
+        self.total_output = total_output
+
+    # determine income:
+    def set_incomes(self):
+        for p in self.get_players():
+            if self.round_number == 2:  # second round is paid to the high wage
+                p.income_strings = p.output * Constants.tokensper_string_high
+                p.income = p.income_strings + p.output_in_switch
+            else:
+                p.income_strings = p.output * Constants.tokensper_string
+                p.income = p.income_strings + p.output_in_switch
+
+
+class Player(BasePlayer):
+    # give each player a letter for recognition (Important for Feedback)
+    def role(self):
+        return string.ascii_uppercase[self.id_in_group - 1]
+
+    # Number of Tasks Solved
+    output0 = models.FloatField(default=0)
+    output = models.FloatField(default=0)
+
+    # available income after solving RET
+    income = models.FloatField(default=0)
+    income_strings = models.FloatField(default=0)
+
+
+    # Variable is 1 when entering switch
+    switch1 = models.PositiveIntegerField(default=0)
+
+    switch_time = models.PositiveIntegerField(default=0)
+
+    time_in_switch = models.PositiveIntegerField(default=0)
+
+    additional_time = models.PositiveIntegerField(default=0)
+
+    output_in_switch = models.FloatField(default=0)
+
+    total_output = models.FloatField(default=0)
+
+    # Time Needed to Solve Task i in Round j with tjxi
+
+    t001 = models.PositiveIntegerField(default=0)
+    t002 = models.PositiveIntegerField(default=0)
+    t003 = models.PositiveIntegerField(default=0)
+    t004 = models.PositiveIntegerField(default=0)
+    t005 = models.PositiveIntegerField(default=0)
+
+    # correct to allow a variable number of tasks
+    # for now, created with:
+    # for i in range(10, 46):
+    # print('t1{} = models.PositiveIntegerField(default=0)'.format(i))
+
+    # def set_time_fields(self):
+    #     for i in range(31):
+    #         self.participant.vars['t1{}'.format(i)] = models.PositiveIntegerField(default=0)
+    #     print(self.participant.vars)
+
+    t101 = models.PositiveIntegerField(default=0)
+    t102 = models.PositiveIntegerField(default=0)
+    t103 = models.PositiveIntegerField(default=0)
+    t104 = models.PositiveIntegerField(default=0)
+    t105 = models.PositiveIntegerField(default=0)
+    t106 = models.PositiveIntegerField(default=0)
+    t107 = models.PositiveIntegerField(default=0)
+    t108 = models.PositiveIntegerField(default=0)
+    t109 = models.PositiveIntegerField(default=0)
+    t110 = models.PositiveIntegerField(default=0)
+    t111 = models.PositiveIntegerField(default=0)
+    t112 = models.PositiveIntegerField(default=0)
+    t113 = models.PositiveIntegerField(default=0)
+    t114 = models.PositiveIntegerField(default=0)
+    t115 = models.PositiveIntegerField(default=0)
+    t116 = models.PositiveIntegerField(default=0)
+    t117 = models.PositiveIntegerField(default=0)
+    t118 = models.PositiveIntegerField(default=0)
+    t119 = models.PositiveIntegerField(default=0)
+    t120 = models.PositiveIntegerField(default=0)
+    t121 = models.PositiveIntegerField(default=0)
+    t122 = models.PositiveIntegerField(default=0)
+    t123 = models.PositiveIntegerField(default=0)
+    t124 = models.PositiveIntegerField(default=0)
+    t125 = models.PositiveIntegerField(default=0)
+    t126 = models.PositiveIntegerField(default=0)
+    t127 = models.PositiveIntegerField(default=0)
+    t128 = models.PositiveIntegerField(default=0)
+    t129 = models.PositiveIntegerField(default=0)
+    t130 = models.PositiveIntegerField(default=0)
+    t131 = models.PositiveIntegerField(default=0)
+    t132 = models.PositiveIntegerField(default=0)
+    t133 = models.PositiveIntegerField(default=0)
+    t134 = models.PositiveIntegerField(default=0)
+    t135 = models.PositiveIntegerField(default=0)
+    t136 = models.PositiveIntegerField(default=0)
+    t137 = models.PositiveIntegerField(default=0)
+    t138 = models.PositiveIntegerField(default=0)
+    t139 = models.PositiveIntegerField(default=0)
+    t140 = models.PositiveIntegerField(default=0)
+    t141 = models.PositiveIntegerField(default=0)
+    t142 = models.PositiveIntegerField(default=0)
+    t143 = models.PositiveIntegerField(default=0)
+    t144 = models.PositiveIntegerField(default=0)
+    t145 = models.PositiveIntegerField(default=0)
+
+    def set_switch1(self):
+        # Time at which switch was entered
+        self.switch_time = self.switch1 * (
+            self.additional_time + self.t101 + self.t102 + self.t103 + self.t104 + self.t105 +
+            self.t106 + self.t107 + self.t108 + self.t109 + self.t110 +
+            self.t111 +
+            self.t112 +
+            self.t113 +
+            self.t114 +
+            self.t115 +
+            self.t116 +
+            self.t117 +
+            self.t118 +
+            self.t119 +
+            self.t120 +
+            self.t121 +
+            self.t122 +
+            self.t123 +
+            self.t124 +
+            self.t125 +
+            self.t126 +
+            self.t127 +
+            self.t128 +
+            self.t129 +
+            self.t130 +
+            self.t131 +
+            self.t132 +
+            self.t133 +
+            self.t134 +
+            self.t135 +
+            self.t136 +
+            self.t137 +
+            self.t138 +
+            self.t139 +
+            self.t140 +
+            self.t141 +
+            self.t142 +
+            self.t143 +
+            self.t144 +
+            self.t145
+        )
+
+        # This variable determines the total time spent in switch
+        self.time_in_switch = self.switch1 * (Constants.t - self.switch_time)
+
+        # This variable determines the tokens per string received
+        self.output_in_switch = self.time_in_switch / Constants.secondsper_token
+
+        # This is the sum of strings + output in switch
+        self.total_output = self.output + self.output_in_switch
---
+++ views.py
@@ -0,0 +1,166 @@
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+import numpy as np
+import string
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+doc = """
+Real Effort Task - Letter Count
+This is the introduction to a Tullock Contest in form of a Real Effort Task
+It welcomes the participants and helps them trough the first stages
+"""
+
+
+# This page does not have a next button and has to be advanced manually.
+# It makes sure, that all participants have roughly the same time for the instructions
+class Welcome(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+# This pages is a rough introduction without instructions
+class Introduction(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class TaskInstructions(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class Round0(Page):
+    form_model = models.Player
+    form_fields = ['t001',
+                   't002',
+                   't003',
+                   't004',
+                   't005',
+                   'output0'
+                   ]
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class WaitingString(WaitPage):
+    def after_all_players_arrive(self):
+        # Make Sure the generating seed depends on the round or session
+        string_list = []  # this will be the list of strings to be used
+        for n in range(10, 200,
+                       Constants.increase_per_string):  # where "a" is the smallest and "b-1" the largest possible string.
+            # For now with an increment of "c"
+            character_list = string.ascii_lowercase  # where the random sample for the strings will be drawn
+            np.random.seed(
+                113 + n + self.round_number)  # on every turn of the loop a new character list will be created
+            character_list += "a" * np.random.randint(15, 30)  # this gives, in average one "a" for each letter
+            np.random.seed(114 + n + self.round_number)  # to ensure reproducibility
+            #  of the alphabet s.t. the relationship is 2:1 but still allows for enough variability
+            string_list.append("".join(np.random.choice(list(character_list), size=n)))
+        self.session.vars['string_list'] = string_list
+        # count solutions
+        a_count = []
+        for i in range(len(string_list)):
+            a_count.append(string_list[i].count("a"))
+        self.session.vars['a_count'] = a_count
+
+
+class SwitchInstructions(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+# correct to allow for variable number of strings
+# for now created with:
+# for i in range(10, 46):
+#    print('\'t1{}\','.format(i))
+
+
+class RET(Page):
+    timeout_seconds = Constants.t
+    form_model = models.Player
+    form_fields = ['t101',
+                   't102',
+                   't103',
+                   't104',
+                   't105',
+                   't106',
+                   't107',
+                   't108',
+                   't109',
+                   't110',
+                   't111',
+                   't112',
+                   't113',
+                   't114',
+                   't115',
+                   't116',
+                   't117',
+                   't118',
+                   't119',
+                   't120',
+                   't121',
+                   't122',
+                   't123',
+                   't124',
+                   't125',
+                   't126',
+                   't127',
+                   't128',
+                   't129',
+                   't130',
+                   't131',
+                   't132',
+                   't133',
+                   't134',
+                   't135',
+                   't136',
+                   't137',
+                   't138',
+                   't139',
+                   't140',
+                   't141',
+                   't142',
+                   't143',
+                   't144',
+                   't145',
+                   'output',
+                   'switch1',
+                   'additional_time'
+                   ]
+
+    def before_next_page(self):
+        self.player.set_switch1()
+
+
+class Waiting(WaitPage):
+    def after_all_players_arrive(self):
+        self.group.set_incomes()
+
+
+class Feedback(Page):
+    pass
+
+
+class HighWageInstructions(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+page_sequence = [
+    #Welcome,
+    #Introduction,
+    #TaskInstructions,
+    #Round0,
+    #WaitingString,
+    #SwitchInstructions,
+    #RET,
+    #Waiting,  # calculates incomes
+    #Feedback,
+    HighWageInstructions,
+]
